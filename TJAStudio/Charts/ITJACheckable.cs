1dcaf10f27962f942ea052fd74854f94d66fc78f
AioiLight <info@aioilight.space>
2019-01-11 21:08:55 +0900

    TJAフォーマットの検証用インターフェイスをつくった

---
+++ TJAStudio/Charts/ITJACheckable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TJAStudio
+{
+    /// <summary>
+    /// TJAフォーマットの検証用インターフェイス。
+    /// </summary>
+    interface ITJACheckable
+    {
+        bool CheckFormat(string text);
+        bool IsAvailable(Simulator simulator);
+        Simulator Simulator { get; }
+        string Format { get; }
+        string Name { get; }
+    }
+}
