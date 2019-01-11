ff51d7117ce382e266c6a45009e0bcf7869727b2
AioiLight <info@aioilight.space>
2019-01-11 19:40:23 +0900

    設定読み書きクラスと設定用のクラスつくった

---
+++ TJAStudio/Settings/SettingManager.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TJAStudio
+{
+    public static class SettingsManager
+    {
+        /// <summary>
+        /// 設定をファイルへ保存します。
+        /// </summary>
+        /// <param name="fileName">ファイル名。</param>
+        /// <param name="setting">Settingクラスのインスタンス。</param>
+        public static void SetSettings(string fileName, Setting setting)
+        {
+            var serializer = new XmlSerializer(typeof(Setting));
+            using (var streamWriter = new StreamWriter(fileName, false, new UTF8Encoding(false)))
+            {
+                serializer.Serialize(streamWriter, setting);
+                serializer = null;
+            }
+        }
+
+        /// <summary>
+        /// 設定をファイルから読み込みます。
+        /// </summary>
+        /// <param name="fileName">ファイル名。</param>
+        /// <returns>Settingクラスのインスタンス。</returns>
+        public static Setting GetSettings(string fileName)
+        {
+            var serializer = new XmlSerializer(typeof(Setting));
+            using (var streamReader = new StreamReader(fileName, new UTF8Encoding(false)))
+            {
+                var setting = (Setting)serializer.Deserialize(streamReader);
+                serializer = null;
+                return setting;
+            }
+        }
+    }
+}
---
+++ TJAStudio/Settings/Settings.cs
@@ -0,0 +1,17 @@
+﻿namespace TJAStudio
+{
+    /// <summary>
+    /// 設定を管理するクラス。
+    /// </summary>
+    public class Setting
+    {
+        /// <summary>
+        /// UIの言語。
+        /// </summary>
+        public string Locale { get; set; } = "ja";
+        /// <summary>
+        /// テキスト表示に使用するフォント名。
+        /// </summary>
+        public string FontName { get; set; } = "MS Gothic";
+    }
+}
