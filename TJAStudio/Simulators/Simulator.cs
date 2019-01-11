2fe438b11b0f7cf2394fd1f7e05451b326608e4e
AioiLight <info@aioilight.space>
2019-01-11 20:59:54 +0900

    シミュレーター列挙体を作った

---
+++ TJAStudio/Simulators/Simulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TJAStudio
+{
+    [Flags]
+    enum Simulator
+    {
+        // PC
+        Taikojiro = 1,
+        Taikojiro2 = 1 << 1,
+        TJAPlayer2fPC = 1 << 2,
+        TJAPlayer3 = 1 << 3,
+        // SP
+        TaikoDaijiro2 = 1 << 4,
+        // Web
+        TaikoWeb_Bui = 1 << 5,
+    }
+}
