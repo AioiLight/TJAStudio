42b97d44bbd1d4684124e0c904ab7becf0a3d420
AioiLight <info@aioilight.space>
2019-01-14 04:11:51 +0900

    りどみ

--- TJAStudio/Windows/Editor.Designer.cs
+++ TJAStudio/Windows/Editor.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             Sgry.Azuki.FontInfo fontInfo1 = new Sgry.Azuki.FontInfo();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Editor));
             this.TextEditor = new Sgry.Azuki.WinForms.AzukiControl();
             this.SuspendLayout();
             // 
@@ -69,6 +70,7 @@
             this.DockAreas = ((WeifenLuo.WinFormsUI.Docking.DockAreas)((WeifenLuo.WinFormsUI.Docking.DockAreas.Float | WeifenLuo.WinFormsUI.Docking.DockAreas.Document)));
             this.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Editor";
             this.Text = "エディター";
