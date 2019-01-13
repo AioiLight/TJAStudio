5798789dd62e5aad090066ddec573702594800b9
AioiLight <info@aioilight.space>
2019-01-14 03:57:13 +0900

    アセンブリインフォの書き換え

--- TJAStudio/Properties/AssemblyInfo.cs
+++ TJAStudio/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TJAStudio")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("(c) AioiLight 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
