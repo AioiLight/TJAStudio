eaa8bb038435492d9266786c696f1db412b3953a
AioiLight <info@aioilight.space>
2019-03-16 03:51:42 +0900

    数値補正とか

--- TJAStudio/DanMarge/NumBoxSuggestion.cs
+++ TJAStudio/DanMarge/NumBoxSuggestion.cs
@@ -3,14 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TJAStudio.DanMarge
 {
     public static class NumBoxSuggestion
     {
-        public static void Suggest(System.Windows.Forms.NumericUpDown numericUpDown, ExamCondition examCondition)
+        public static void SuggestConsiderExamCondition(NumericUpDown numericUpDown, ExamCondition.ExamConditions examCondition)
         {
+            void Set(int min, int max)
+            {
+                numericUpDown.Minimum = min;
+                numericUpDown.Maximum = max;
+            }
+            switch (examCondition)
+            {
+                case ExamCondition.ExamConditions.Gauge:
+                    Set(1, 100);
+                    break;
+                case ExamCondition.ExamConditions.Perfect:
+                case ExamCondition.ExamConditions.Good:
+                case ExamCondition.ExamConditions.Bad:
+                case ExamCondition.ExamConditions.Score:
+                case ExamCondition.ExamConditions.Drumroll:
+                case ExamCondition.ExamConditions.Hits:
+                case ExamCondition.ExamConditions.Combo:
+                    Set(1, int.MaxValue);
+                    break;
+                default:
+                    Set(1, 100);
+                    break;
+            }
+        }
+        
+        public static void SuggestConsiderRedRequirement(NumericUpDown red, NumericUpDown gold, ExamScope.ExamScopes examScopes)
+        {
+            switch (examScopes)
+            {
+                case ExamScope.ExamScopes.More:
+                    if(red.Value > gold.Value)
+                    {
+                        // ターゲットの数値が考慮するやつより大きかったら
+                        red.Value = gold.Value;
+                    }
+                    break;
+                case ExamScope.ExamScopes.Less:
+                    if(red.Value < gold.Value)
+                    {
+                        gold.Value = red.Value;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
 
+        public static void SuggestConsiderGoldRequirement(NumericUpDown gold, NumericUpDown red, ExamScope.ExamScopes examScopes)
+        {
+            switch (examScopes)
+            {
+                case ExamScope.ExamScopes.More:
+                    if (gold.Value < red.Value)
+                    {
+                        // ターゲットの数値が考慮するやつより大きかったら
+                        gold.Value = red.Value;
+                    }
+                    break;
+                case ExamScope.ExamScopes.Less:
+                    if (gold.Value > red.Value)
+                    {
+                        red.Value = gold.Value;
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
