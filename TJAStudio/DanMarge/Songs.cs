38734b4335b317a5792739192a1660bb999edef9
AioiLight <info@aioilight.space>
2019-03-17 01:47:14 +0900

    一応課題曲管理用クラス作っておく

---
+++ TJAStudio/DanMarge/Songs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TJAStudio.DanMarge
+{
+    public class Songs
+    {
+        public string FilePath { get; set; }
+        public string Chart { get; set; }
+    }
+}
