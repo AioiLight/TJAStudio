b95a132088e35aeeb3598f472f27079058d51497
AioiLight <info@aioilight.space>
2019-09-18 21:17:11 +0900

    使っていないusing文の削除

--- TJAStudio/Dialog.cs
+++ TJAStudio/Dialog.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.WindowsAPICodePack;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace TJAStudio
