4ef9fa96e5ce157fb2b3a455f1da61887ae514c1
AioiLight <info@aioilight.space>
2020-07-18 16:38:34 +0900

    不要なusing削除

--- TJAStudio/BPMMeasure/BPMMeasure.cs
+++ TJAStudio/BPMMeasure/BPMMeasure.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Net;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TJAStudio.BPMMeasure
